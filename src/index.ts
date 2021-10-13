5f35bceba4795d2cf168625ff09ac6e896c429d9
wangxingkang <156148958@qq.com>
2021-10-13 12:20:11 +0800

    feat: 支持 prefix、suffix

--- src/index.ts
+++ src/index.ts
@@ -16,23 +16,49 @@ export function numberFormat(options: Options = {}) {
   const opts = Object.assign({}, defaultOptions, options);
   validateOptions(opts);
 
+  const { prefix, suffix, template } = opts;
   const { thousandSeparator, decimalSeparator } = getSeparators(opts);
 
-  opts.thousandSeparator = thousandSeparator;
-
-
-  const format = (value: number) => {
-    let { beforeDecimal, afterDecimal, addNegation } = splitDecimal(value + '', opts.allowNegative);
+  const formatAsNumber = (numStr: string) => {
+    let { beforeDecimal, afterDecimal, addNegation } = splitDecimal(numStr + '', opts.allowNegative);
 
     if (thousandSeparator) {
       beforeDecimal = applyThousandSeparator(beforeDecimal, thousandSeparator);
     }
 
+    /** add prefix and suffix */
+    if (prefix) beforeDecimal = prefix + beforeDecimal;
+    if (suffix) afterDecimal = afterDecimal + suffix;
+
+    /** restore negation sign */
+    if (addNegation) beforeDecimal = '-' + beforeDecimal;
 
     return beforeDecimal + afterDecimal;
   }
 
+  const formatNumString = (numStr: string = '') => {
+    let formattedValue = numStr;
+
+    if (numStr === '') {
+      formattedValue = '';
+    } else {
+      formattedValue = formatAsNumber(formattedValue);
+    }
+
+    return formattedValue;
+  }
+
+  const format = (val: number) => {
+    if (!template) {
+      return formatNumString(val + '');
+    }
+
+    return ''
+  }
+
   return {
-    format
+    format,
   }
 }
+
+export default numberFormat;
--- test/index.test.ts
+++ test/index.test.ts
@@ -16,5 +16,20 @@ describe('numberFormat', () => {
       const result = numberFormat({ thousandSeparator: '*' }).format(123456);
       expect(result).toEqual('123*456');
     });
+
+    it('负数', () => {
+      const result = numberFormat().format(-123456);
+      expect(result).toEqual('-123,456');
+    });
+
+    it(`添加前缀`, () => {
+      const result = numberFormat({ prefix: '$' }).format(123456);
+      expect(result).toEqual('$123,456');
+    });
+
+    it(`添加后缀`, () => {
+      const result = numberFormat({ suffix: '/100' }).format(98);
+      expect(result).toEqual('98/100');
+    });
   });
 });
